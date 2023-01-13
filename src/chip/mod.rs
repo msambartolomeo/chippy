3291db48d4a1e15827e0c0d4c10b575720550384
Mauro Sambartolomeo <67807065+msambartolomeo@users.noreply.github.com>
2023-01-13 18:15:55 -0300

    Move memory and stack to its own modules

--- src/chip/mod.rs
+++ src/chip/mod.rs
@@ -1,34 +1,17 @@
 mod display;
 mod keyboard;
+mod memory;
+mod stack;
 mod timer;
 
 use display::Display;
 use keyboard::Keyboard;
+use memory::Memory;
+use stack::Stack;
 use timer::{Delay, Sound};
 
-const MAX_MEMORY: usize = 4096;
 const ROM_START: u16 = 0x200;
 const REGISTERS_COUNT: usize = 16;
-const STACK_SIZE: usize = 16;
-
-const DEFAULT_SPRITES: [u8; 5 * 16] = [
-    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-    0x20, 0x60, 0x20, 0x20, 0x70, // 1
-    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
-];
 
 // s or start - A 4-bit value, the first 4 bits of the instruction
 // nnn or addr - A 12-bit value, the lowest 12 bits of the instruction
@@ -46,40 +29,30 @@ enum BitVariables {
 }
 
 pub struct Chip {
-    memory: [u8; MAX_MEMORY],
     v_registers: [u8; REGISTERS_COUNT],
     i_register: u16,
     program_counter_register: u16,
-    stack_pointer_register: u8,
+    memory: Memory,
     delay_timer: Delay,
     sound_timer: Sound,
-    stack: [u16; STACK_SIZE],
+    stack: Stack,
     keyboard: Keyboard,
     display: Display,
 }
 
 impl Chip {
     pub fn new() -> Chip {
-        let mut chip = Chip {
-            memory: [0; MAX_MEMORY],
+        Chip {
             v_registers: [0; REGISTERS_COUNT],
             i_register: 0,
-            program_counter_register: 0,
-            stack_pointer_register: 0,
+            program_counter_register: ROM_START,
+            memory: Memory::new(),
             delay_timer: Delay::default(),
             sound_timer: Sound::default(),
-            stack: [0; STACK_SIZE],
+            stack: Stack::new(),
             keyboard: Keyboard::default(),
             display: Display::new(),
-        };
-
-        for (i, byte) in DEFAULT_SPRITES.iter().enumerate() {
-            chip.memory[i] = *byte;
         }
-
-        chip.program_counter_register = 0x200;
-
-        chip
     }
 
     // Returns the part of the u16 represented by the variable
---
+++ src/chip/stack.rs
@@ -0,0 +1,34 @@
+const STACK_SIZE: usize = 16;
+
+pub struct Stack {
+    array: [u16; STACK_SIZE],
+    stack_pointer: usize,
+}
+
+impl Stack {
+    pub fn new() -> Stack {
+        Stack {
+            array: [0; STACK_SIZE],
+            stack_pointer: 0,
+        }
+    }
+
+    pub fn push(&mut self, value: u16) {
+        if self.stack_pointer >= STACK_SIZE {
+            panic!("Stack is full");
+        }
+        self.array[self.stack_pointer] = value;
+        self.stack_pointer += 1;
+    }
+
+    pub fn pop(&mut self) -> u16 {
+        if self.stack_pointer <= 0 {
+            panic!("Stack is empty");
+        }
+
+        let value = self.array[self.stack_pointer];
+        self.stack_pointer -= 1;
+
+        value
+    }
+}
