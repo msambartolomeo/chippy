e8372903227757c6cd624af2f74db707ea75c8f8
Mauro Sambartolomeo <67807065+msambartolomeo@users.noreply.github.com>
2023-01-13 22:10:14 -0300

    Add instructions

--- src/chip/mod.rs
+++ src/chip/mod.rs
@@ -41,10 +41,26 @@ impl Chip {
         }
     }
 
+    #[inline]
+    fn increase_pc(&mut self) {
+        self.pc_register += 2;
+    }
+
+    #[inline]
+    fn set_flag(&mut self, condition: bool) {
+        self.v_registers[0xF] = match condition {
+            true => 1,
+            false => 0,
+        };
+    }
+
     fn process_instruction(&mut self) {
         let instruction = Instruction::from(self.pc_register);
 
         let nibbles = instruction.get_nibbles();
+
+        let v_x = self.v_registers[instruction.x];
+        let v_y = self.v_registers[instruction.y];
 
         match nibbles {
             // 00E0 - CLS
@@ -56,35 +72,66 @@ impl Chip {
             // 1nnn - JP addr
             (0x1, _, _, _) => self.pc_register = instruction.nnn,
             // 2nnn - CALL addr
-            (0x2, _, _, _) => todo!(),
+            (0x2, _, _, _) => {
+                self.stack.push(self.pc_register);
+                self.pc_register = instruction.nnn;
+            }
             // 3xkk - SE Vx, byte
-            (0x3, _, _, _) => todo!(),
+            (0x3, _, _, _) => {
+                if v_x == instruction.kk {
+                    self.increase_pc();
+                }
+            }
             // 4xkk - SNE Vx, byte
-            (0x4, _, _, _) => todo!(),
+            (0x4, _, _, _) => {
+                if v_x != instruction.kk {
+                    self.increase_pc();
+                }
+            }
             // 5xy0 - SE Vx, Vy
-            (0x5, _, _, 0x0) => todo!(),
+            (0x5, _, _, 0x0) => {
+                if v_x == v_y {
+                    self.increase_pc();
+                }
+            }
             // 6xkk - LD Vx, byte
-            (0x6, _, _, _) => todo!(),
+            (0x6, _, _, _) => self.v_registers[instruction.x] = instruction.kk,
             // 7xkk - ADD Vx, byte
-            (0x7, _, _, _) => todo!(),
+            (0x7, _, _, _) => self.v_registers[instruction.x] += instruction.kk,
             // 8xy0 - LD Vx, Vy
-            (0x8, _, _, 0x0) => todo!(),
+            (0x8, _, _, 0x0) => self.v_registers[instruction.x] = v_y,
+
             // 8xy1 - OR Vx, Vy
-            (0x8, _, _, 0x1) => todo!(),
+            (0x8, _, _, 0x1) => self.v_registers[instruction.x] |= v_y,
             // 8xy2 - AND Vx, Vy
-            (0x8, _, _, 0x2) => todo!(),
+            (0x8, _, _, 0x2) => self.v_registers[instruction.x] &= v_y,
             // 8xy3 - XOR Vx, Vy
-            (0x8, _, _, 0x3) => todo!(),
+            (0x8, _, _, 0x3) => self.v_registers[instruction.x] ^= v_y,
             // 8xy4 - ADD Vx, Vy
-            (0x8, _, _, 0x4) => todo!(),
+            (0x8, _, _, 0x4) => {
+                self.set_flag(v_x as u16 + v_y as u16 > 255);
+                self.v_registers[instruction.x] = v_x.wrapping_add(v_y);
+            }
             // 8xy5 - SUB Vx, Vy
-            (0x8, _, _, 0x5) => todo!(),
+            (0x8, _, _, 0x5) => {
+                self.set_flag(v_x > v_y);
+                self.v_registers[instruction.x] = v_x.wrapping_sub(v_y);
+            }
             // 8xy6 - SHR Vx {, Vy}
-            (0x8, _, _, 0x6) => todo!(),
+            (0x8, _, _, 0x6) => {
+                self.set_flag(v_x & 0x01 == 1);
+                self.v_registers[instruction.x] = v_x >> 1;
+            }
             // 8xy7 - SUBN Vx, Vy
-            (0x8, _, _, 0x7) => todo!(),
+            (0x8, _, _, 0x7) => {
+                self.set_flag(v_y > v_x);
+                self.v_registers[instruction.x] = v_y.wrapping_sub(v_x);
+            }
             // 8xyE - SHL Vx {, Vy}
-            (0x8, _, _, 0xE) => todo!(),
+            (0x8, _, _, 0xE) => {
+                self.set_flag(v_x >> 7 == 1);
+                self.v_registers[instruction.x] = v_x << 1;
+            }
             // 9xy0 - SNE Vx, Vy
             (0x9, _, _, 0x0) => todo!(),
             // Annn - LD I, addr
